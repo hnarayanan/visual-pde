37ed5f0145d79d91278a6b254894ccdc28adc01d
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2023-02-05 15:25:30 +0000

    Updated to Ben's latest version

--- sim/scripts/RD/post_shaders.js
+++ sim/scripts/RD/post_shaders.js
@@ -1,22 +1,24 @@
 // post_shaders.js
 
-export function computeDisplayFunShader() {
+export function computeDisplayFunShaderTop() {
     return `varying vec2 textureCoords;
     uniform sampler2D textureSource;
-
     void main()
     {
+       `
+}
+
+export function computeDisplayFunShaderBot() {
+    return `
         vec4 uvw = texture2D(textureSource, textureCoords);
         float value = FUN;
         gl_FragColor = vec4(value, 0.0, 0.0, 1.0);
-
     }`
 }
 
 export function computeMaxSpeciesShader() {
     return `varying vec2 textureCoords;
     uniform sampler2D textureSource;
-
     void main()
     {
         float value = 0.0;
